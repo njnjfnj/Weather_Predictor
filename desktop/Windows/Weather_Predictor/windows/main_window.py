b6ea549e95b27736e3a4f5288effb72b3430663b
Serhii <asus147538297319@gmail.com>
2024-01-27 19:29:35 +0200

    initial code

---
+++ desktop/Windows/Weather_Predictor/windows/main_window.py
@@ -0,0 +1,15 @@
+# This Python file uses the following encoding: utf-8
+#from PySide6 import QtCore
+from PySide6 import QtWidgets
+#from PySide6 import QtCore
+from PySide6 import QtGui
+
+#class Widget(QWidget):
+#    def __init__(self, parent=None):
+#        super().__init__(parent)
+
+class Main_window(QtWidgets.QWidget):
+    def __init__(self, screen, parent=None):
+        super().__init__(parent)
+
+
