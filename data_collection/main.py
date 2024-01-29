5bdec2f99f13b265ca4da7ca11d0bc4194510cf8
soloclimb <keito.eternal@gmail.com>
2024-01-29 23:28:07 +0200

    data collection in .csv files instead of json - Now we're getting only needed parameters, in more compact,readable way - Temperature - converted in Celsius - Wind speed in km/h

--- data_collection/main.py
+++ data_collection/main.py
@@ -4,7 +4,6 @@ from os import path, mkdir
 from dotenv import load_dotenv, dotenv_values
 from datetime import date, datetime, timedelta
 from time import sleep, mktime
-import json
 load_dotenv()
 
 def to_gmt(dt):
@@ -16,8 +15,30 @@ API_KEY = dotenv_values()['API_KEY']
 CITY_DATA_DIR='./city_weather_datasets/'
 
 datetime_format = '%Y-%m-%d %H:%M:%S'
-START_DATE = datetime.strptime('2023-02-02 00:00:00', datetime_format)
-END_DATE = to_gmt(datetime.strptime('2023-02-03 00:00:00', datetime_format))
+START_DATE = datetime.strptime('2023-01-02 00:00:00', datetime_format)
+END_DATE = to_gmt(datetime.strptime('2023-04-02 00:00:00', datetime_format))
+ 
+CITY_WEATHER_COLUMNS = ['timestamp','temp','feels_like','pressure','humidity','temp_min','temp_max','wind_speed','wind_deg','clouds_coverage', 'weather_category','weather_description']
+
+def transform_weather_json(json_str):
+    info = json_str['list'][0]
+    timestamp = info['dt']
+    temp = float(info['main']['temp']) - 273.15 
+    feels_like = float(info['main']['feels_like']) - 273.15
+    pressure = info['main']['pressure']
+    humidity = info['main']['humidity']
+    temp_min = float(info['main']['temp_min']) - 273.15
+    temp_max = float(info['main']['temp_max']) - 273.15
+    wind_speed = info['wind']['speed'] * 3.6
+    wind_deg = info['wind']['deg']
+    clouds_coverage = info['clouds']['all']
+    weather_category = info['weather'][0]['main']
+    weather_description = info['weather'][0]['main']
+
+    arr = [timestamp, temp, feels_like, pressure, humidity, temp_min, temp_max, wind_speed, wind_deg, clouds_coverage, weather_category, weather_description]
+    
+    return arr
+
 
 
 with open('cities/cities.csv', 'r') as cities_csv:
@@ -38,10 +59,9 @@ with open('cities/cities.csv', 'r') as cities_csv:
             START_DATE = NEXT_DATETIME
 
             res = requests.get(url=url).json()
-            print(res)
-            arr.append(res)
-            sleep(0.5)
+            transofmed_weather = transform_weather_json(res)
 
+            arr.append(transofmed_weather)
 
         
         if path.isdir(CITY_DATA_DIR):
@@ -49,20 +69,22 @@ with open('cities/cities.csv', 'r') as cities_csv:
             if not path.isdir(city_directory):
                 mkdir(city_directory)
 
-            filename = path.join(city_directory, city[name_index].lower() + '.json')
+            filename = path.join(city_directory, city[name_index].lower() + '.csv')
             existing_data = []
             
             try:
                 with open(filename, 'r') as city_file:
-                        content = city_file.read()
-                        if content:
-                            existing_data = json.loads(content)
-            
+                    reader = csv.reader(city_file)
+                    existing_data = list(reader)
             except FileNotFoundError:
                 existing_data = []
 
+            if not existing_data[0] == CITY_WEATHER_COLUMNS:
+                                existing_data.insert(0, CITY_WEATHER_COLUMNS)
+
             existing_data.extend(arr)
 
-            with open(filename, 'w') as city_file:
-                json.dump(existing_data, city_file, indent=2)
+            with open(filename, 'w', newline='') as city_file:
+                writer = csv.writer(city_file)
+                writer.writerows(existing_data)
 
