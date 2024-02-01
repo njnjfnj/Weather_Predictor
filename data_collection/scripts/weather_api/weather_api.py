ba67a851df2425bed169915c136700c4fb3223dc
soloclimb <keito.eternal@gmail.com>
2024-02-01 16:19:26 +0200

    refactor changes in API interaction scripts

--- data_collection/scripts/weather_api/weather_api.py
+++ data_collection/scripts/weather_api/weather_api.py
@@ -1,10 +1,29 @@
-import requests
-from datetime import timedelta
-from time import mktime
+import csv
+from dotenv import load_dotenv, dotenv_values
+from datetime import datetime, timedelta
 
-def get_city_weather_data(city, start_date, next_datetime, api_key):
-    time_difference = int(city['time_difference'][1:]) if city['time_difference'][0] == '-' else int(city['time_difference'])
-    adjusted_start_date = start_date - timedelta(hours=time_difference)
-    hour = int(next_datetime.strftime('%H')) + time_difference
-    url = f'https://history.openweathermap.org/data/2.5/history/city?lat={city["lat"]}&lon={city["lon"]}&type=hour&start={(mktime(adjusted_start_date.timetuple()))}&end={(mktime(next_datetime.timetuple()))}&appid={api_key}'
-    return requests.get(url=url).json()
+from load.load import update_city_info
+
+def load_api_key():
+    load_dotenv()
+    return dotenv_values()['API_KEY']
+
+API_KEY = load_api_key()
+CITY_DATA_DIR='./city_weather_datasets/'
+
+datetime_format = '%Y-%m-%d %H:%M:%S'
+
+UTC_NOW = datetime.utcnow()
+LOCAL_NOW = datetime.now()
+TIME_DIFFERENCE = int((LOCAL_NOW - UTC_NOW).total_seconds() / 3600)
+
+START_DATE = datetime.strptime('2023-07-28 00:00:00', datetime_format) + timedelta(hours=TIME_DIFFERENCE)
+END_DATE = datetime.strptime('2023-12-31 00:00:00', datetime_format) 
+ 
+CITY_WEATHER_COLUMNS = ['timestamp','temp','feels_like','pressure','humidity','temp_min','temp_max','wind_speed','wind_deg','clouds_coverage', 'weather_category','weather_description']
+
+with open('cities/cities.csv', 'r') as cities_file:
+    cities = list(csv.reader(cities_file, delimiter=',', quotechar='"'))
+    cities_columns =  cities[0]
+    for i in range(1, len(cities)):
+        update_city_info(START_DATE, END_DATE, cities[i], cities_columns, CITY_WEATHER_COLUMNS)
