c87a1752447f3737c1be2c3275be71fa24e4875e
soloclimb <keito.eternal@gmail.com>
2024-02-04 23:34:47 +0200

    models now are saved in json(best practise prohet) - Figured out that reccomended way in docs is saving to json - Used built in prophet functions to do it

--- data_collection/scripts/model_training/utils/utils.py
+++ data_collection/scripts/model_training/utils/utils.py
@@ -1,10 +1,9 @@
-import pickle
+from prophet.serialize import model_to_json, model_from_json
 
 def save_model(model, filename):
-    with open(filename, 'wb') as file:
-        pickle.dump(model, file)
-
+    with open(filename, 'w') as fout:
+        fout.write(model_to_json(model)) 
+        
 def load_model(filename):
-    with open(filename, 'rb') as file:
-        model = pickle.load(file)
-    return model
+    with open(filename, 'r') as fin:
+        return model_from_json(fin.read())
