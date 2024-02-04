237d5c458a386095bc192e68a9452c5d3b14559b
soloclimb <keito.eternal@gmail.com>
2024-02-04 23:34:47 +0200

    models now are saved in json(best practise prohet) - Figured out that reccomended way in docs is saving to json - Used built in prophet functions to do it

--- data_collection/scripts/model_training/model_training.py
+++ data_collection/scripts/model_training/model_training.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from dotenv import load_dotenv, dotenv_values
 from .humidity.humidity import create_humidity_model
 from .wind_speed.wind_speed import create_wind_speed_model
 from .pressure.pressure import create_pressure_model
@@ -7,31 +8,35 @@ from .temp.temp import create_temp_max_model
 from .temp.temp import create_temp_model
 from .weather_category.weather_category import create_weather_category_model
 
+load_dotenv()
+CITIES_WEATHER_DATA_DIR = dotenv_values()['CITIES_WEATHER_DATA_DIR']
+CITIES_WEATHER_MODELS_DIR = dotenv_values()['CITIES_WEATHER_MODELS_DIR']
+
 PRODUCTS = ['humidity', 'pressure', 'temp', 'temp_min', 'temp_max', 'wind_speed', 'weather_category']
 
 def create_products_models(city_name, test_size):
-    df = pd.read_csv(f'city_weather_datasets/{city_name}/{city_name}.csv')
+    df = pd.read_csv(f'{CITIES_WEATHER_DATA_DIR}/{city_name}/{city_name}.csv')
 
     for product in PRODUCTS:
 
         df = df.rename(columns={'timestamp': 'ds', product: 'y'})
         df['ds'] = pd.to_datetime(df['ds'], unit='s')
 
-        filename = f'city_weather_models/{city_name}/{product}.pkl'
+        filename = f'{CITIES_WEATHER_MODELS_DIR}/{city_name}/{product}.json'
 
         match product:
-            # case 'humidity':
-            #     create_humidity_model(df, test_size, filename)
-            # case 'wind_speed':
-            #     create_wind_speed_model(df, test_size, filename)
-            # case 'pressure':
-            #     create_pressure_model(df, test_size, filename)
-            # case 'temp_min':
-            #     create_temp_min_model(df, test_size, filename)
-            # case 'temp_max':
-            #     create_temp_max_model(df, test_size, filename)
-            # case 'temp':
-            #     create_temp_model(df, test_size, filename) 
+            case 'humidity':
+                create_humidity_model(df, test_size, filename)
+            case 'wind_speed':
+                create_wind_speed_model(df, test_size, filename)
+            case 'pressure':
+                create_pressure_model(df, test_size, filename)
+            case 'temp_min':
+                create_temp_min_model(df, test_size, filename)
+            case 'temp_max':
+                create_temp_max_model(df, test_size, filename)
+            case 'temp':
+                create_temp_model(df, test_size, filename) 
             case 'weather_category':
                 create_weather_category_model(df, filename)
         
