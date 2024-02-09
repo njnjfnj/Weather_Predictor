a4d5f48dee486f859f04b03bdd248d1c244c37f3
soloclimb <keito.eternal@gmail.com>
2024-02-09 23:19:15 +0200

    now flask api works in container(returns predicts)

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,14 @@ RUN mkdir api && mkdir scripts
 
 COPY api ./api/
 COPY scripts ./scripts/
+COPY data ./data/
 COPY __init__.py ./__init__.py
+
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
 ENV FLASK_APP=./api/app.py
 EXPOSE 4000
 
-CMD ["flask", "run", "--host=0.0.0.0", "--port=4000"]
+CMD ["flask", "run", "--host=0.0.0.0", "--port=4000", "--debug"]
 
--- api/app.py
+++ api/app.py
@@ -1,13 +1,12 @@
 import flask
 from flask import Response
-from ..scripts.model_prediction.model_prediction import predict_city_weather    
+from ..scripts.model_prediction.model_prediction import predict_city_weather
 app = flask.Flask(__name__)
 
 @app.route("/predict/<city_name>/<prediction_hours>")
 def predict_weather(city_name, prediction_hours):
-    # csv_data = predict_city_weather(city_name=city_name, prediction_hours=prediction_hours)
-    response = Response(f"{city_name},{prediction_hours}", content_type="text/csv")
+    csv_data = predict_city_weather(city_name=city_name, prediction_hours=prediction_hours)
+    response = Response(csv_data, content_type="text/csv")
     response.headers["Content-Disposition"] = "attachment; filename=weather.csv"
-    return response
+    return response, 200
      
-
