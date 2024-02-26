50cb247b9f0c5d75e75b0e4e2f1ea8fe6028b712
soloclimb <keito.eternal@gmail.com>
2024-02-26 19:37:22 +0200

    fixed cities name match issue - Created route to return all cities

--- redis_scripts/get.py
+++ redis_scripts/get.py
@@ -12,20 +12,47 @@ def connect_to_redis(host, port):
 
 cursor = 0
 
+def construct_result(arr, e=''):
+    if arr:
+        return dumps({"result": arr, "status": "success"})
+    else:
+        message = e if e != '' else "No matching cities found"
+        return dumps({"result": arr, "status": "error", "message": message})
+
+
 def get_city(city_name):
     r = connect_to_redis(host="redis", port="6379")
-    city_name = city_name.lower().capitalize() + "*"
+    city_name = city_name.replace("_", " ")
+    splitted = city_name.split(" ")
+    prepared_name = ''
+    for e in splitted:
+        prepared_name += e.lower().capitalize() + " "
     res = []
-    c, keys = list(r.zscan(name="city_names", cursor=0, match=city_name))  
-    for key, index in keys:
-        arr = r.hmget(name=key, keys=tuple(hash_table_keys))
-        res.append([key.decode("UTF-8")])
-        res[-1] = res[-1] + [e.decode("UTF-8") for e in arr]
+    prepared_name = prepared_name.strip() + "*"
     try:
-        if res:
-            return dumps({"match": res})
-        else:
-            return dumps({"error": "No matching cities found"})
+        c, keys = list(r.zscan(name="city_names", cursor=0, match=prepared_name))  
+        for key, index in keys:
+            arr = r.hmget(name=key, keys=tuple(hash_table_keys))
+        
+            res.append([key.decode("UTF-8")])
+            res[-1] = res[-1] + [e.decode("UTF-8") for e in arr]
+        
+        return construct_result(res)
+
     except Exception as e:
-        print(f"Error during get request: {e}")
-        return dumps({"error": "An error occurred"})
+        return construct_result(res, e)
+    
+def get_all_cities():
+    r = connect_to_redis(host="redis", port="6379")
+    res = []
+    try:
+        c, keys = list(r.zscan(name="city_names", cursor=0, match="*"))  
+        for key, index in keys:
+            arr = r.hmget(name=key, keys=tuple(hash_table_keys))
+            res.append([key.decode("UTF-8")])
+            res[-1] = res[-1] + [e.decode("UTF-8") for e in arr]
+            
+        return construct_result(res)
+    
+    except Exception as e:
+        return construct_result(res, e)
