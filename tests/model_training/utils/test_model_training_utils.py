bf4b02c0e0edb6bee6e54c474889ffed94bd88f2
soloclimb <keito.eternal@gmail.com>
2024-04-02 09:58:55 +0300

    updating repo on pull

--- tests/model_training/utils/test_model_training_utils.py
+++ tests/model_training/utils/test_model_training_utils.py
@@ -1,131 +1,79 @@
-import pandas as pd
-
-import pytest
-
 from prophet import Prophet
 from prophet.serialize import model_from_json, model_to_json
+from src.scripts.model_training.utils.utils import save_model, load_model
+import pytest as pt
+from unittest.mock import patch
+import pandas as pd
 
-from src.scripts.model_training.utils.utils import (
-    save_prophet_model, load_prophet_model, save_sklearn_model, load_sklearn_model)
+from json import JSONDecodeError
 
-from json import dump, JSONDecodeError
-
-from sklearn.tree import DecisionTreeClassifier
-from random import choice
-
-from pickle import UnpicklingError
-
-@pytest.fixture
-def prophet_model():
+def test_save_model_success():
     model = Prophet()
     model.fit(pd.DataFrame({'ds': pd.date_range(start='2012-01-01', end='2012-02-01'), 'y': range(0, 32)}))
-    return model
-   
-@pytest.fixture
-def sklearn_model():
-    x = pd.DataFrame({"wind_speed": range(20), "temp": range(20)})
-    y = pd.DataFrame({"y": [choice(["Clouds", "Sun", "Rain"]) for _ in range(20)]})
-    model = DecisionTreeClassifier(random_state=0)
-    model.fit(x, y)
-    return model
+    filename ="test.json"
+    save_model(model, str(filename))
+    with open(filename, 'r') as m:
+        m = model_from_json(m.read())
+    assert m;
 
-@pytest.fixture
-def get_model_by_type(prophet_model, sklearn_model):
-    def _get_model_by_type(name):
-        if name == "Prophet": return prophet_model
-        else: return sklearn_model
-    return _get_model_by_type
+def test_save_model_file_error():
+    model = Prophet()
+    model.fit(pd.DataFrame({'ds': pd.date_range(start='2012-01-01', end='2012-02-01'), 'y': range(0, 32)}))
+    filename ="/test.json"
 
-@pytest.mark.parametrize(
-    ("model_type", "save_function", "load_function", "filename"),
-    [
-        ("Prophet", save_prophet_model, load_prophet_model, "test.json"),
-        ("Sklearn", save_sklearn_model, load_sklearn_model, "test.pkl"),
-    ],
-)
-def test_save_model_success(model_type ,save_function, load_function,
-                             filename, tmp_path, get_model_by_type):
-    filename = tmp_path / filename
-    model = get_model_by_type(model_type)
-    save_function(model, str(filename))
-    m = load_function(str(filename))
-    assert m is not None
-
-@pytest.mark.parametrize(("model_type", "save_function", "filename"),
-                         [("Prophet", save_prophet_model, "test.json"),
-                         ("Sklearn", save_sklearn_model, "test.pkl")])
-def test_save_model_filepath_error(model_type, save_function, filename,
-                             tmp_path, get_model_by_type):
-    model = get_model_by_type(model_type)
-    filename = "/" + filename 
-    filename = tmp_path / filename
-    with pytest.raises(PermissionError):
-        save_function(model, str(filename))
+    with pt.raises(PermissionError):
+        save_model(model, str(filename))
         
-    filename = tmp_path / "/abc/dea/test.json" if model_type == "Prophet" else "/abc/dea/test.pkl"
+    filename ="/abc/dea/test.json"
 
-    with pytest.raises(FileNotFoundError):
-        save_function(model, str(filename))
+    with pt.raises(FileNotFoundError):
+        save_model(model, str(filename))
 
-
-@pytest.mark.parametrize(("model_type", "save_function", "filename"),
-                         [("Sklearn", save_sklearn_model, "test.pkl"),
-                          ("Prophet", save_prophet_model , "test.json")
-                         ])
-def test_save_model_convertion_error(model_type, save_function,
-                             filename, tmp_path):
-    model =  Prophet() if model_type == "Prophet" else DecisionTreeClassifier()
-    filename = tmp_path / filename
-    err = AttributeError if model_type == "Sklearn" else ValueError
-    with pytest.raises(err):
-        save_function(model, str(filename))
+def test_save_model_convertion_error():
+    model = Prophet()
+    filename = "test.json"
+    with pt.raises(ValueError):
+        save_model(model, str(filename))
 
     model = None
-    err = AttributeError if model_type == "Sklearn" else AttributeError
-    with pytest.raises(err):
-        save_function(model, str(filename))
-
-@pytest.mark.parametrize(("model_type", "save_function", "load_function", "filename"), 
-                         [("Prophet", save_prophet_model, load_prophet_model, "test.json"),
-                         ("Sklearn",  save_sklearn_model, load_sklearn_model, "test.pkl")])
-def test_load_model_success(model_type, save_function, load_function, filename,
-                             tmp_path, get_model_by_type):
-    filename = tmp_path / filename
-    model = get_model_by_type(model_type)
-    save_function(model, filename)
-    m = load_function(filename)
-    assert m
-
-@pytest.mark.parametrize(("model_type", "save_function", "load_function", "filename", "open_extension"), 
-                         [("Prophet", save_prophet_model, load_prophet_model, "test.json", "r+"),
-                         ("Sklearn",  save_sklearn_model, load_sklearn_model, "test.pkl", "rb+")])
-def test_load_model_conversion_error(model_type, save_function, load_function, filename,
-                                     open_extension,  tmp_path, get_model_by_type):
-    filename = tmp_path / filename
-    model = get_model_by_type(model_type)
-    save_function(model, filename)
-    with open(filename, open_extension) as f:
-        f.truncate(0)
-    err = (UnpicklingError, EOFError) if model_type == "Sklearn" else JSONDecodeError
-    with pytest.raises(err):
-        load_function(str(filename))
+    filename = "test.json"
+    with pt.raises(AttributeError):
+        save_model(model, str(filename))
 
 
-@pytest.mark.parametrize(("model_type", "save_function", "load_function", "filename"),
-                         [("Prophet", save_prophet_model,load_prophet_model , "test.json"),
-                         ("Sklearn",  save_sklearn_model, load_sklearn_model, "test.pkl")])
-def test_load_model_filepath_error(model_type, save_function, load_function, 
-                                   filename, tmp_path, get_model_by_type):
-    filename = tmp_path / filename
-    model = get_model_by_type(model_type)
-    save_function(model, filename)
+    model = Prophet()
+    model.fit(pd.DataFrame({'ds': pd.date_range(start='2012-01-01', end='2012-02-01'), 'y': range(0, 32)}))
+    save_model(model, str(filename))
 
-    filename = "/test.json" if model_type == "Prophet" else "/test.pkl"
+def load_test_model(filename):    
+    model = Prophet()
+    model.fit(pd.DataFrame({'ds': pd.date_range(start='2012-01-01', end='2012-02-01'), 'y': range(0, 32)}))
+    with open(filename, 'w') as fout:
+        fout.write(model_to_json(model))
 
-    with pytest.raises(FileNotFoundError):
-        load_function(str(filename))
+def test_load_model_success():
+    filename = 'test.json'
+    load_test_model(filename)
+    m = load_model(filename)
+    assert m;
 
-    filename = "/abc/dea/test.json" if model_type == "Prophet" else "/abc/dea/test.pkl"
+def test_load_model_convertion_error():
+    filename = 'test.json'
+    load_test_model(filename)
+    with open(filename, "r+") as f:
+        f.truncate()
+    with pt.raises(JSONDecodeError):
+        m = load_model(filename)
+    
+def test_load_model_filepath_error():
+    filename = 'test.json'
+    load_test_model(filename)
+    filename = '/test.json'
 
-    with pytest.raises(FileNotFoundError):
-        load_function(str(filename))
+    with pt.raises(FileNotFoundError):
+        m = load_model(str(filename))
+        
+    filename ="/abc/dea/test.json"
+
+    with pt.raises(FileNotFoundError):
+        m = load_model(str(filename))
